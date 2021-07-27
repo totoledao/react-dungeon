8fc3ae28de93a254178d04b6d292687d5796fda5
Guilherme Toledo <totoledao@hotmail.com>
2021-07-27 14:59:31 -0300

    Added custom modules

---
+++ custom.d.ts
@@ -0,0 +1,13 @@
+  
+declare module '*.png' {
+  const content: any;
+  export default content;
+}
+declare module '*.jpg' {
+  const content: any;
+  export default content;
+}
+declare module '2d-dungeon' {
+  const content: any;
+  export default content;
+}
