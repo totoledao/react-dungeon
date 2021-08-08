2d399e96ca4b02b5db5061830ea9c4e68cdaf15c
Guilherme Toledo <totoledao@hotmail.com>
2021-08-08 12:03:30 -0300

    Starting to implement enemy AI

--- src/components/Enemy.tsx
+++ src/components/Enemy.tsx
@@ -1,4 +1,3 @@
-import { forwardRef, useImperativeHandle, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 import ActorProps from '../types/Actor';
@@ -6,38 +5,12 @@ import Image from 'next/image';
 
 import imp from '../sprites/imp_idle_anim_f0.png';
 
-export type EnemyMovement = {
-  moveEnemy : () => void;
-}
-
-const Enemy : React.ForwardRefRenderFunction<EnemyMovement> =
-  ({ Xpos , Ypos, handleInteraction } : ActorProps , ref ) => {
-
-    const [enemyTurns, setEnemyTurns] = useState(0);
-
-    const updatedXpos = useRef<number | null>(Xpos);
-    const updatedYpos = useRef<number | null>(Ypos);
-
-    function moveEnemy() {
-      console.log("Moving...");        
-      updatedXpos.current = 10;
-      updatedYpos.current = 10;
-      setEnemyTurns(enemyTurns +1)
-      console.log(updatedXpos,updatedYpos)
-      return {
-        updatedXpos,
-        updatedYpos
-      }
-    }
-
-    useImperativeHandle(ref, () => ({
-        moveEnemy,
-    }));
+export default function Enemy ({ Xpos , Ypos, handleInteraction } : ActorProps ) {
 
     const Enemy = styled.div`
     position: absolute;
-    top: ${updatedXpos.current*32}px;
-    left: ${updatedYpos.current*32}px;  
+    top: ${Xpos*32}px;
+    left: ${Ypos*32}px;  
     transform: translateY(-2px);
     z-index: 90;
     `;
@@ -49,5 +22,3 @@ const Enemy : React.ForwardRefRenderFunction<EnemyMovement> =
     ) 
   
 }
-
-export default forwardRef(Enemy);
---
+++ src/utils/enemyTurn.tsx
@@ -0,0 +1,5 @@
+import { currentPlayerPosX, currentPlayerPosY, currentEnemiesPos } from '../../pages/';
+
+export default function enemyTurn ( ) {
+  console.log(currentEnemiesPos);
+}
