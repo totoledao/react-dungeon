a2738dd44a635b4278e15cf9ff49b27387348dbf
Guilherme Toledo <totoledao@hotmail.com>
2021-08-08 12:02:37 -0300

    Refactored random genaration

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,80 +8,29 @@ import TileWall from "../src/components/TileWall";
 
 //actors
 import Player from "../src/components/Player";
-import Enemy, {EnemyMovement} from '../src/components/Enemy';
+import Enemy from '../src/components/Enemy';
 
 //utils
 import playerMovement from '../src/utils/playerMovement';
 import enemyInteraction from '../src/utils/enemyInteraction';
+import enemyTurn from '../src/utils/enemyTurn';
 
-//Player Position
+//exports
 export let currentPlayerPosX : number;
 export let currentPlayerPosY : number;
+export let currentEnemiesPos : { i: number, j: number}[];
 
 export default function Home() {
 
   const [floors, setFloors] = useState<{ i: number, j: number}[]>([]);
   const [walls, setWalls] = useState<{ i: number, j: number}[]>([]);
+  const [enemies, setEnemies] = useState<{ i: number, j: number}[]>([]);
   const [playerPos, setPlayerPos] = useState<{ i: number, j: number}>({i: 10, j: 10});
   const [dungeonLevel, setDungeonLevel] = useState( 1 );
 
   currentPlayerPosX =  playerPos.i;
   currentPlayerPosY =  playerPos.j;
-
-  const enemyPosRef = useRef<EnemyMovement>();
- 
-  //const handleEnemyMovement = enemyPosRef.current?.moveEnemy();
-
-  const floorsTiling = React.useMemo(
-    () =>( 
-    floors.map((num) =>
-      <TileFloor key={`${num.i}-${num.j}`}
-        Xpos={num.i} Ypos={num.j}
-        //handleMovement={() => setPlayerPos({i: num.i, j: num.j})}
-        handleMovement={() => setPlayerPos(oldValue => playerMovement(oldValue, {i: num.i, j: num.j} ) ) }
-      /> )
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  ),[floors])
-  
-  const wallsTiling = React.useMemo(
-    () =>( 
-    walls.map((num) =>
-      <TileWall key={`${num.i}-${num.j}`}
-        Xpos={num.i} Ypos={num.j}
-        handleMovement={() => console.log({i: num.i, j: num.j})}
-      /> )
-  ),[walls])
-
-  const enemiesPlacement = React.useMemo(
-    () =>( 
-    floors.map((num) => {
-      if(Math.random() >= 0.94) {
-        return (       
-        <Enemy key={ `imp-${num.i}-${num.j}` }
-        Xpos={num.i} Ypos={num.j}
-        ref={ enemyPosRef }
-        handleInteraction={ () => enemyInteraction( {i: num.i, j: num.j} ) }
-        />
-      )}
-  })
-  ),[floors])
-
-  // const enemiesPlacement = React.useMemo(
-  //   () =>( 
-  //   floors.map((num) => Math.random() >= 0.9
-  //     ?
-  //     <Enemy key={ `imp-${num.i}-${num.j}` }
-  //       Xpos={num.i} Ypos={num.j}
-  //       handleInteraction={ () => enemyInteraction( {i: num.i, j: num.j} ) }
-  //     />
-  //     :
-  //     console.log("Not here at "+ num.i +" " + num.j)
-  //     )
-  // ),[floors])
-
-  useEffect(() => {
-    DungeonGenerator(29, 29);
-  },[ setDungeonLevel ]);
+  currentEnemiesPos = enemies;  
 
   function DungeonGenerator(Xsize: number, Ysize: number){
 
@@ -112,14 +61,41 @@ export default function Home() {
   });
   
   dungeon.generate();
-  dungeon.print(); //outputs wall map to console.log  
+  //dungeon.print(); //outputs wall map to console.log  
+
+    let totalFloors : { i: number, j: number}[] = [];
+    let totalWalls : { i: number, j: number}[] = [];
+    let cleanedUpFloors : { i: number, j: number}[] = [];
 
     for (let i = 0; i < Xsize; i++) {    
       for (let j = 0; j < Ysize; j++) {
         if ( dungeon.walls.get([i, j]) ) {
-          setWalls(oldValue => [ ...oldValue, {i,j}]);        
-        } else setFloors(oldValue => [ ...oldValue, {i,j}]);
+          setWalls(oldValue => [ ...oldValue, {i,j}]);
+          totalWalls.push( {i,j} );      
+        } else totalFloors.push( {i,j} );          
       }
+    }
+
+    for (let x = 0; x < totalFloors.length; x++) {
+      
+      let i = totalFloors[x].i;
+      let j = totalFloors[x].j;      
+       
+        if (totalFloors[x].i < totalWalls[totalWalls.length - 1].i) {        
+          if (totalFloors[x].j < totalWalls[totalWalls.length - 1].j) {        
+            setFloors( oldValue => [ ...oldValue, {i,j}] );
+            cleanedUpFloors.push( {i,j} );
+        }}
+    }
+    
+    for (let x = 0; x < cleanedUpFloors.length; x++) {
+      
+      let i = cleanedUpFloors[x].i;
+      let j = cleanedUpFloors[x].j;      
+       
+        if (Math.random() >= 0.94) {    
+            setEnemies( oldValue => [ ...oldValue, {i,j}] );            
+        }
     }
 
     for(let piece of dungeon.children) {
@@ -137,33 +113,51 @@ export default function Home() {
 
   }
 
-  function moveEnemies () {
-    console.log(enemiesPlacement);
-    enemiesPlacement.map( (obj) => {
-      obj?.props.Xpos.value + 1 ;
-    });
-  }
+  const floorsTiling = React.useMemo(
+    () =>( 
+    floors.map((num) =>
+      <TileFloor key={`${num.i}-${num.j}`}
+        Xpos={num.i} Ypos={num.j}        
+        handleMovement={() => setPlayerPos(oldValue => playerMovement(oldValue, {i: num.i, j: num.j} ) ) }
+      /> )
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  ),[floors]);
+  
+  const wallsTiling = React.useMemo(
+    () =>( 
+    walls.map((num) =>
+      <TileWall key={`${num.i}-${num.j}`}
+        Xpos={num.i} Ypos={num.j}
+        handleMovement={() => console.log({i: num.i, j: num.j})}
+      /> )
+  ),[walls]);
+
+  const enemiesPlacement = React.useMemo(
+    () =>( 
+    enemies.map((num) =>    
+        <Enemy key={ `imp-${num.i}-${num.j}` }
+          Xpos={num.i} Ypos={num.j}
+          handleInteraction={ () => enemyInteraction( {i: num.i, j: num.j} ) }
+        />
+      )
+  ),[enemies]);
+
+  useEffect(() => {
+    DungeonGenerator(29, 29);
+  },[setDungeonLevel]);
 
   return (
-    <main>
-    
+    <main>    
+
     { floorsTiling }
     { wallsTiling }
     { enemiesPlacement }
-
-    {/* <Enemy key={ "imp-0" }
-         Xpos={10} Ypos={10}
-         handleInteraction={ () => enemyInteraction( {i: 10, j: 10} ) }
-       /> */}
+    
+    <Player Xpos={playerPos.i} Ypos={playerPos.j} handleInteraction={ () => console.log("That's you!") }/>
 
     <button style={{position: "absolute", left: "1000px" }}
-      onClick={() => enemiesPlacement.map(() => {        
-        enemyPosRef.current?.moveEnemy();
-      })
-    }
+      onClick={ () => enemyTurn() }
     > MOVE </button>
-
-    <Player Xpos={playerPos.i} Ypos={playerPos.j} handleInteraction={ () => console.log("That's you!") }/>
      
     </main>
   )
