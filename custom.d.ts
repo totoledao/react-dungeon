d23cc77d9f79635cbc95676945333491db520b1e
Guilherme Toledo <totoledao@hotmail.com>
2021-08-30 18:58:47 -0300

    Image properties

--- custom.d.ts
+++ custom.d.ts
@@ -1,12 +1,3 @@
-  
-declare module '*.png' {
-  const content: any;
-  export default content;
-}
-declare module '*.jpg' {
-  const content: any;
-  export default content;
-}
 declare module '2d-dungeon' {
   const content: any;
   export default content;
